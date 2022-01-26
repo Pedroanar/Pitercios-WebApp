856aa9b889da55ed7d6b47e733d4c03b1771bfa2
Pedro <98405052+Pedroanar@users.noreply.github.com>
2022-01-26 14:16:29 +0100

    component rrss ok

--- src/App2.js
+++ src/App2.js
@@ -1,11 +1,14 @@
 import HEADER from "./components/header";
 import RRSS from "./components/rrss";
+import "./index.css";
 
 export default function App2() {
   return (
-    <>
-      <HEADER />
-      <RRSS />
-    </>
+    <div className="page">
+      <div className="container">
+        <HEADER />
+        <RRSS />
+      </div>
+    </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,16 @@
 import "../index.css";
 import React from "react";
-import ImgIni from "../imagenes/PiT3Inicial.png";
 
 export default function HEADER() {
   return (
-    <div className="page">
-      <div className="container">
-        <img className="imgprin" src={ImgIni} alt="imgprin" />
-        <p className="nombre">Pitercios</p>
-        <p className="eslogan">El aula de aprender</p>
-      </div>
-    </div>
+    <>
+      <img
+        className="imgprin"
+        src={"/imagenes/PiT3Inicial.png"}
+        alt="imgprin"
+      />
+      <p className="nombre">Pitercios</p>
+      <p className="eslogan">El aula de aprender</p>
+    </>
   );
 }
